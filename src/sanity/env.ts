fbfe3cecba7b28daf10d71f054a23adee49d9c56
vikaswakde <vikaswakdepc@gmail.com>
2024-08-09 00:28:41 +0530

    env changes

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -3,12 +3,15 @@ export const apiVersion =
 
 export const dataset = assertValue(
   // process.env.NEXT_PUBLIC_SANITY_DATASET,
-  process.env.SANITY_STUDIO_DATASET || "production",
+  process.env.SANITY_STUDIO_DATASET ||
+    "production" ||
+    process.env.NEXT_PUBLIC_SANITY_DATASET,
   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+    process.env.SANITY_STUDIO_PROJECT_ID,
   // process.env.SANITY_STUDIO_PROJECT_ID,
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
