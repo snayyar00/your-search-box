5e10352452aba2c514d317e984661ae68351441b
vikaswakde <vikaswakdepc@gmail.com>
2024-08-11 02:37:52 +0530

    fixed unessary button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
         <Testimonials />
         <FYQ />
         <BlogCards />
-        <CreatePostsButton />
+        {/* <CreatePostsButton /> */}
       </TracingBeam>
       <CallToAction />
       <Footer />
