76555857d6271f9a4da7fb94e4b7df9d6f51374e
Junaid083 <iamjunaid021@gmail.com>
2024-09-18 16:23:46 +0500

    ADDED: signle showcase page SEO added

--- src/app/(website-pages)/showcase/[slug]/page.tsx
+++ src/app/(website-pages)/showcase/[slug]/page.tsx
@@ -9,6 +9,7 @@ import { sanityFetch } from "@/sanity/lib/fetch";
 import { client } from "@/sanity/lib/client";
 // import Post from "../components/Post";
 import ShowCasePost from "../components/ShowcasePost";
+import { Metadata } from "next";
 
 export const revalidate = 60;
 
@@ -16,7 +17,35 @@ export async function generateStaticParams() {
   const posts = await client.fetch(showcasePathsQuery);
   return posts;
 }
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const post = await sanityFetch<SanityDocument>({
+    query: showcaseQuery,
+    params,
+  });
+  return {
+    title: `Your Search Box ${post?.baselink}`,
+    description: post?.questions[0] || "",
+    openGraph: {
+      locale: "en_US",
+      type: "article",
+      title: `Your Search Box ${post?.baselink}`,
+      description: post?.questions[0] || "",
+      siteName: "Your Search Box",
 
+      images: [
+        {
+          url: "https://your-search-box.vercel.app/og-image.png",
+          width: 1200,
+          height: 627,
+          alt: "Your search box - Effective AI Search.",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+    },
+  };
+}
 const PostPage = async ({ params }: { params: any }) => {
   const post = await sanityFetch<SanityDocument>({
     query: showcaseQuery,
