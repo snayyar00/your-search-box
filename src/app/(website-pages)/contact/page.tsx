0561c93cc2a07d5df1ad0923616366c82b6ee2ca
Junaid083 <iamjunaid021@gmail.com>
2024-09-18 16:57:31 +0500

    UPDATE: contact us page uupdated with metaData

--- src/app/(website-pages)/contact/page.tsx
+++ src/app/(website-pages)/contact/page.tsx
@@ -1,157 +1,66 @@
-"use client";
+import ContactUsComponenet from "@/app/components/ContactUs";
+import { Metadata } from "next";
 
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { Send, Phone, Mail, MapPin } from "lucide-react";
-import Image from "next/image";
-import AnimatedHeader from "@/components/custom-animations/AnimatedHeader";
-import BackgroundGridImage from "@/assets/why-choose-us-grid.svg";
-
-import contactGif from "@/assets/contact.gif";
+export const metadata: Metadata = {
+  title: "Get in Touch with Us | Your Search Box",
+  description:
+    "Have questions or need assistance? Reach out to the Your Search Box team through our contact form. We're here to help.",
+  openGraph: {
+    title: "Get in Touch with Us | Your Search Box",
+    description:
+      "Contact the Your Search Box team for any inquiries or support. We're ready to assist you with all your questions.",
+    url: "https://your-website.com/contact",
+    images: [
+      {
+        url: "https://your-website.com/og-image-contact.png",
+        width: 1200,
+        height: 627,
+        alt: "Contact Your Search Box",
+      },
+    ],
+    type: "website",
+    locale: "en_US",
+    siteName: "Your Search Box",
+  },
+  metadataBase: {
+    host: "https://your-search-box.vercel.app",
+    href: "/",
+    origin: "https://your-search-box.vercel.app",
+    password: "your-search-box",
+    hash: "your-search-box",
+    pathname: "/",
+    search: "",
+    username: "your-search-box",
+    hostname: "your-search-box.vercel.app",
+    port: "",
+    protocol: "https:",
+    searchParams: new URLSearchParams(""),
+    toString: () => "https://your-search-box.vercel.app/",
+    toJSON: () => "https://your-search-box.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@yourwebsite", // Your website's Twitter handle
+    creator: "@yourwebsite", // Creator's Twitter handle
+    title: "Get in Touch with Us | Your Search Box",
+    description:
+      "Reach out to Your Search Box for assistance or support. Our team is here to help with any inquiries.",
+    images: [
+      {
+        url: "https://your-website.com/og-image-contact.png",
+        width: 1200,
+        height: 627,
+        alt: "Contact Your Search Box",
+      },
+    ],
+  },
+};
 
 const ContactUs = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    console.log(formData);
-    // Reset form after submission
-    setFormData({ name: "", email: "", message: "" });
-  };
-
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#14093E] to-[#0E0627] text-white relative">
-      <div className="w-full absolute left-0 -top-[400px] min-h-96 ">
-        <BackgroundGridImage className="w-full h-full opacity-60" />
-      </div>
-      <div className="container mx-auto px-4 py-16 relative ">
-        <motion.h1
-          className="text-4xl md:text-5xl font-bold text-center mb-12"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <AnimatedHeader
-            className="section-header pt-5 from-white to-[#586285] text-transparent bg-clip-text
-            [-webkit-background-clip:text] py-5 lg:text-7xl lg:tracking-wide"
-          >
-            Get in Touch
-          </AnimatedHeader>
-        </motion.h1>
-
-        <div className="grid md:grid-cols-2 gap-12 my-24">
-          <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-          >
-            <h2 className="text-2xl font-semibold mb-6">Contact Information</h2>
-            <div className="space-y-4">
-              <div className="flex items-center">
-                <Phone className="mr-4 text-blue-400" />
-                <span>+1 (778) 636-0039</span>
-              </div>
-              <div className="flex items-center">
-                <Mail className="mr-4 text-blue-400" />
-                <span>support@yoursearchbox.com</span>
-              </div>
-              <div className="flex items-center">
-                <MapPin className="mr-4 text-blue-400" />
-                <span>Canada</span>
-              </div>
-            </div>
-            <div className="mt-12">
-              <Image
-                src={contactGif}
-                alt="Icons by Lordicon.com Illustration"
-                width={230}
-                height={230}
-                loading="lazy"
-                placeholder="blur"
-                blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-              />
-            </div>
-          </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.4 }}
-          >
-            <form onSubmit={handleSubmit} className="space-y-6">
-              <div>
-                <label htmlFor="name" className="block mb-2">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 rounded bg-[#1F1147] border border-[#3D2F6F] focus:outline-none focus:border-blue-500 transition duration-300"
-                  aria-label="name"
-                />
-              </div>
-              <div>
-                <label htmlFor="email" className="block mb-2">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 rounded bg-[#1F1147] border border-[#3D2F6F] focus:outline-none focus:border-blue-500 transition duration-300"
-                  aria-label="email"
-                />
-              </div>
-              <div>
-                <label htmlFor="message" className="block mb-2">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                  rows={4}
-                  className="w-full px-4 py-2 rounded bg-[#1F1147] border border-[#3D2F6F] focus:outline-none focus:border-blue-500 transition duration-300"
-                ></textarea>
-              </div>
-              <motion.button
-                type="submit"
-                className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl transition duration-300 flex items-center justify-center"
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                Send Message
-                <Send className="ml-2" size={18} />
-              </motion.button>
-            </form>
-          </motion.div>
-        </div>
-      </div>
-    </div>
+    <>
+      <ContactUsComponenet />
+    </>
   );
 };
 
